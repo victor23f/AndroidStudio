e30542bc47c787aba6481977893fa34f1d42ebeb
victor23f <81162838+victor23f@users.noreply.github.com>
2022-09-30 13:43:25 +0200

    VIDEO17

--- app/src/main/java/com/example/primeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/primeraaplicacion/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.primeraaplicacion;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -10,37 +11,41 @@ public class MainActivity extends Activity {
 
     public int contador;
     public EditText et1;
+    TextView resultadoTexto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+         resultadoTexto = (TextView)findViewById(R.id.contadorNumero);
+         contador=0;
     }
     public void incrementar(View vista){
         contador++;
-        mostrarRestultado();
+        resultadoTexto.setText( "" + contador);
     }
     public void resetear(View vista){
         et1 = (EditText)findViewById(R.id.n_reseteo);
 
         contador= Integer.parseInt(et1.getText().toString());
 
-        mostrarRestultado();
+        et1.setText("");
+
+        resultadoTexto.setText( "" + contador);
     }
     public void decrementar(View vista){
         contador--;
-        mostrarRestultado();
-    }
-    public void mostrarRestultado(){
-        TextView resultadoTexto = (TextView)findViewById(R.id.contadorNumero);
-        resultadoTexto.setText( "" + contador);
-
-    }
-    public void permitirNegativos(View vista){
-            if (contador<0){
+            CheckBox negativos = (CheckBox)findViewById(R.id.checkBoxNegativos);
+            if (!negativos.isChecked()&& (Integer.parseInt(resultadoTexto.getText().toString())==0)){
                 contador=0;
             }
-    mostrarRestultado();
+
+        resultadoTexto.setText( "" + contador);
     }
+  //  public void mostrarRestultado(){
+    //    TextView resultadoTexto = (TextView)findViewById(R.id.contadorNumero);
+      //  resultadoTexto.setText( "" + contador);
+
+    //}
+
 
 }
