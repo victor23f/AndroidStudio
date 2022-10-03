52f31d8fd4ad1022a53afff8cf5b6d5d187c635c
victor23f <81162838+victor23f@users.noreply.github.com>
2022-10-03 20:56:02 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/primeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/primeraaplicacion/MainActivity.java
@@ -1,8 +1,9 @@
-package com.example.primeraaplicacion;
+package com.example.PrimeraAplicacion;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,44 +13,47 @@ public class MainActivity extends Activity {
     public int contador;
     public EditText et1;
     TextView resultadoTexto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         resultadoTexto = (TextView)findViewById(R.id.contadorNumero);
-         contador=0;
+        resultadoTexto = (TextView) findViewById(R.id.contadorNumero);
+        contador = 0;
     }
-    public void incrementar(View vista){
-        contador++;
-        resultadoTexto.setText( "" + contador);
-    }
-    public void resetear(View vista){
-        et1 = (EditText)findViewById(R.id.n_reseteo);
 
-        contador= Integer.parseInt(et1.getText().toString());
+    public void incrementar(View vista) {
+        contador++;
+        resultadoTexto.setText("" + contador);
+    }
+
+    public void resetear(View vista) {
+        et1 = (EditText) findViewById(R.id.n_reseteo);
+
+        contador = Integer.parseInt(et1.getText().toString());
 
         et1.setText("");
 
-        resultadoTexto.setText( "" + contador);
+        resultadoTexto.setText("" + contador);
+
+        InputMethodManager miteclado = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        miteclado.hideSoftInputFromWindow(et1.getWindowToken(),0);
     }
+
     public void decrementar(View vista) {
         contador--;
-        if (contador<0){
+        if (contador < 0) {
             CheckBox negativo = (CheckBox) findViewById(R.id.checkBoxNegativos);
-            if(!negativo.isChecked()){
-                contador=0;
+            if (!negativo.isChecked()) {
+                contador = 0;
             }
         }
-        mostrarResultado();
+        mostrarRestultado();
     }
-
-        resultadoTexto.setText( "" + contador);
-    }
-    public void mostrarRestultado(){
-        TextView resultadoTexto = (TextView)findViewById(R.id.contadorNumero);
-        resultadoTexto.setText( "" + contador);
+    public void mostrarRestultado() {
+        TextView resultadoTexto = (TextView) findViewById(R.id.contadorNumero);
+        resultadoTexto.setText("" + contador);
 
     }
-
 
 }
