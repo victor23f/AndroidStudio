1425ab73ccb1d2aea80ca1400a338f20cf31f8aa
victor23f <81162838+victor23f@users.noreply.github.com>
2022-10-05 09:22:15 +0200

    radiobuttons

--- app/src/main/java/com/example/primeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/primeraaplicacion/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.primeraaplicacion;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -12,40 +14,79 @@ public class MainActivity extends Activity {
     public int contador;
     public EditText et1;
     TextView resultadoTexto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         resultadoTexto = (TextView)findViewById(R.id.contadorNumero);
-         contador=0;
+        resultadoTexto = (TextView) findViewById(R.id.contadorNumero);
+        contador = 0;
     }
-    public void incrementar(View vista){
-        contador++;
-        resultadoTexto.setText( "" + contador);
-    }
-    public void resetear(View vista){
-        et1 = (EditText)findViewById(R.id.n_reseteo);
 
-        contador= Integer.parseInt(et1.getText().toString());
+    public void incrementar(View vista) {
+        contador++;
+        if (contador>0) {
+            RadioButton positivo = (RadioButton) findViewById(R.id.positivos);
+            positivo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (!positivo.isSelected()) {
+                        positivo.setChecked(true);
+                        positivo.setSelected(true);
+                    } else {
+                        positivo.setChecked(false);
+                        positivo.setSelected(false);
+                    }
+                }
+            });
+            if (!positivo.isChecked()) {
+                contador = 0;
+            }
+        }
+        mostrarRestultado();
+    }
+
+    public void resetear(View vista) {
+        et1 = (EditText) findViewById(R.id.n_reseteo);
+
+        contador = Integer.parseInt(et1.getText().toString());
 
         et1.setText("");
 
-        resultadoTexto.setText( "" + contador);
+        resultadoTexto.setText("" + contador);
+
+        InputMethodManager miteclado = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        miteclado.hideSoftInputFromWindow(et1.getWindowToken(),0);
     }
-    public void decrementar(View vista){
+
+    public void decrementar(View vista) {
         contador--;
-            CheckBox negativos = (CheckBox)findViewById(R.id.checkBoxNegativos);
-            if (!negativos.isChecked()&& (Integer.parseInt(resultadoTexto.getText().toString())==0)){
-                contador=0;
+        if (contador < 0) {
+            RadioButton negativo = (RadioButton) findViewById(R.id.negativos);
+            negativo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (!negativo.isSelected()) {
+                        negativo.setChecked(true);
+                        negativo.setSelected(true);
+                    } else {
+                        negativo.setChecked(false);
+                        negativo.setSelected(false);
+                    }
+                }
+            });
+            if (!negativo.isChecked()) {
+                contador = 0;
             }
-
-        resultadoTexto.setText( "" + contador);
+        }
+        mostrarRestultado();
     }
-  //  public void mostrarRestultado(){
-    //    TextView resultadoTexto = (TextView)findViewById(R.id.contadorNumero);
-      //  resultadoTexto.setText( "" + contador);
 
-    //}
+    public void mostrarRestultado() {
+        TextView resultadoTexto = (TextView) findViewById(R.id.contadorNumero);
+        resultadoTexto.setText("" + contador);
+
+    }
 
 
 }
